7934d57f75363b3dfcbae696f4c6f65f4c3786ff
Jamesgit-8710 <84719466+Jamesgit-8710@users.noreply.github.com>
2023-08-02 18:35:05 +0530

    updated

--- openApi/backend/server.js
+++ openApi/backend/server.js
@@ -6,8 +6,7 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/test", async (req, res) => {
-    console.log(req.body.user)
-  res.status(201).send(`New user created successfully! \n Name : ${req.body.user} \n Password : ${req.body.pass}`);
+  res.status(201).send({user: req.body.user, pass: req.body.pass});
 });
 
 app.listen(8000, () => {
