bc98c45107a0453afcb63c918242455ef58610b0
Jamesgit-8710 <84719466+Jamesgit-8710@users.noreply.github.com>
2023-08-08 18:21:40 +0530

    updated

---
+++ Docer/frontend/dockerapp/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div style={{height: "100vh", width: "100vw", backgroundColor: "#F3F7FB", display: "flex", justifyContent: "center", alignItems: "center"}}>
+      Docker App
+    </div>
+  )
+}
+
+export default App
